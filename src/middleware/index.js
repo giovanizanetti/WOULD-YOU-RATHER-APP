c3192af62d30d8ddf19d9acdada7c06501407785
giovanizanetti <zanetti.giovani@gmail.com>
2021-01-15 09:37:10 +0100

    chore: add redux devtools

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,5 +1,6 @@
 import thunk from 'redux-thunk'
 import logger from './logger'
 import { applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
-export default applyMiddleware(thunk, logger)
+export default composeWithDevTools(applyMiddleware(thunk, logger))
