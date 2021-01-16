c5886f9aa56f51c3f597c8d0d40375b71304f85b
giovanizanetti <zanetti.giovani@gmail.com>
2021-01-16 16:34:24 +0100

    style: add background oppacity

--- src/components/NavBar/MobileDrawer.js
+++ src/components/NavBar/MobileDrawer.js
@@ -1,40 +1,43 @@
 import { NavLink } from 'react-router-dom'
-const MobileDrawer = () => {
+const MobileDrawer = ({ show, setShow }) => {
   return (
-    <nav className='mobileNav'>
-      <img
-        src='./assets/humbuguer icon.svg'
-        alt='oprn menu button'
-        style={{ margin: '1.5rem', display: 'flex', alignSelf: 'flex-start', width: 30 }}
-      />
-      <div className='mobileDrawer'>
-        <div className='mobileUser'>
-          <img src='./assets/avata-m-1.svg' style={{ width: '6rem', margin: '1rem' }} alt='user'></img>
+    <div className='mobileNav' style={{ background: 'rgb(51 51 51 / .6)' }}>
+      {show && (
+        <nav className='mobileDrawer'>
+          <img
+            onClick={() => setShow(!show)}
+            src='./assets/close-24px.svg'
+            alt='oprn menu button'
+            style={{ margin: '1rem', display: 'flex', alignSelf: 'flex-end', width: 30, cursor: 'pointer' }}
+          />
+          <div className='mobileUser'>
+            <img src='./assets/avata-m-1.svg' style={{ width: '6rem', margin: '1rem' }} alt='user'></img>
 
-          <span>Hello User</span>
-        </div>
-        <ul>
-          <li>
-            <NavLink to='/'>
-              <div>
-                <img src=''></img>
-                <span>Home</span>
-              </div>
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to='/newQuestion'>New Question</NavLink>
-          </li>
-          <li>
-            <NavLink to='/leaderboard'>Leader Board</NavLink>
-          </li>
+            <span>Hello User</span>
+          </div>
+          <ul>
+            <li>
+              <NavLink to='/'>
+                <div>
+                  <img src=''></img>
+                  <span>Home</span>
+                </div>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to='/newQuestion'>New Question</NavLink>
+            </li>
+            <li>
+              <NavLink to='/leaderboard'>Leader Board</NavLink>
+            </li>
 
-          <li>
-            <NavLink to='signin'>Logout</NavLink>
-          </li>
-        </ul>
-      </div>
-    </nav>
+            <li>
+              <NavLink to='signin'>Logout</NavLink>
+            </li>
+          </ul>
+        </nav>
+      )}
+    </div>
   )
 }
 
