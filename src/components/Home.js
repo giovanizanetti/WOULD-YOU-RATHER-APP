c6701cbefc67fda24a6b561c3c096e5d2beb3ffa
giovanizanetti <zanetti.giovani@gmail.com>
2021-01-23 12:39:49 +0100

    style: atyle question container

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,2 +1,24 @@
-const Home = () => <div>Home</div>
+const Home = () => {
+  return (
+    <div className='container'>
+      <nav>
+        <span>Answered</span>
+        <span>Unanswered</span>
+      </nav>
+      <div className='container'>
+        <div className='container-header question-card-header'>Paulinho Asks:</div>
+        <div className='question-card-outer flex-row pad-TB-1'>
+          <img className='question-card-avatar' src='./assets/avata-m-1.svg' alt='user avatar' />
+          <div className='flex-column question-card-inner'>
+            <h3>Would You Rather?</h3>
+            <span>...blalala...</span>
+            <button className='btn btn-transparent' style={{}}>
+              View poll
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
 export default Home
