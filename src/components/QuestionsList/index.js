72debcfdcc54e571fefc2ddbb4329405f4c07eb6
giovanizanetti <zanetti.giovani@gmail.com>
2021-01-26 09:38:49 +0100

    feat: sort questions decresent

--- src/components/QuestionsList/index.js
+++ src/components/QuestionsList/index.js
@@ -38,11 +38,15 @@ const QuestionsList = ({ activeList }) => {
 
   // Check which list needs the questions and pass the correct questions
   const filteredQuestions = activeList === 'answered' ? answeredQuestions : unansweredQuestions
-
+  const sortedQuestions =
+    filteredQuestions.length && filteredQuestions.sort((a, b) => questions[a].timestamp - questions[b].timestamp)
+  // Sort questions with most the recent at the top
+  // const sortedQuestions = filteredQuestions.map(question)
   return (
     <div style={{ overflowY: 'auto', position: 'relative' }}>
-      {filteredQuestions.length ? (
-        filteredQuestions.map((question) => <QuestionListItem key={question} question={questions[question]} />)
+      {console.log(sortedQuestions)}
+      {sortedQuestions.length ? (
+        sortedQuestions.map((question) => <QuestionListItem key={question} question={questions[question]} />)
       ) : (
         <strong className='flex-column noQuestion-msg'>There are no {activeList} questions</strong>
       )}
